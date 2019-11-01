aa0aa11dd18528544e6f1b75297ea9aea67347ff
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2019-11-01 19:15:26 +0600

    Add Http Request and Response Test

---
+++ src/main/java/com/madbarsoft/doctorchamber/abLearnHttp/EmployeeDto.java
@@ -0,0 +1,22 @@
+package com.madbarsoft.doctorchamber.abLearnHttp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EmployeeDto {
+	
+	private int id;
+	
+	private String name;
+	
+	private String gender;
+	
+	private int age;
+	
+	
+
+}
--- src/main/java/com/madbarsoft/doctorchamber/abLearnHttp/TestHttpCall.java
+++ src/main/java/com/madbarsoft/doctorchamber/abLearnHttp/TestHttpCall.java
@@ -7,52 +7,52 @@ import java.net.URL;
 
 public class TestHttpCall {
 	
-	private static HttpURLConnection conn;
-	
-
-	public static void main(String[] args) {
-
-       System.out.println("============ CALL HTTP REQUEST ===============");
-       
-       
-       StringBuffer responseContent = new StringBuffer();
-       BufferedReader reader;
-       String line;
-       
-       try {
-    	   
-    	   URL url = new URL("http://localhost:8090/api/consulation/list");
-    	   conn = (HttpURLConnection) url.openConnection();
-    	   
-    	   
-    	  conn.setRequestMethod("GET");
-    	  conn.setConnectTimeout(5000);
-    	  conn.setReadTimeout(5000);
-    	  
-    	  int reqStatus = conn.getResponseCode();
-    	  System.out.println("REQ STATUS# "+reqStatus);
-    	  
-    	  
-    	  reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-          
-    	  while ((line = reader.readLine()) != null ){
-    		  responseContent.append(line);
-    	  }
-    	  
-    	  System.out.println("REQ RESPONSE# "+responseContent);
-    	   
-    	  
-    	   
-    	   
-    	   
-       }catch (Exception e){
-    	   e.printStackTrace();
-       }finally {
-    	   conn.disconnect();
-	   }
-	}
-	
-	
+//	private static HttpURLConnection conn;
+//	
+//
+//	public static void main(String[] args) {
+//
+//       System.out.println("============ CALL HTTP REQUEST ===============");
+//       
+//       
+//       StringBuffer responseContent = new StringBuffer();
+//       BufferedReader reader;
+//       String line;
+//       
+//       try {
+//    	   
+//    	   URL url = new URL("http://localhost:8090/api/consulation/list");
+//    	   conn = (HttpURLConnection) url.openConnection();
+//    	   
+//    	   
+//    	  conn.setRequestMethod("GET");
+//    	  conn.setConnectTimeout(5000);
+//    	  conn.setReadTimeout(5000);
+//    	  
+//    	  int reqStatus = conn.getResponseCode();
+//    	  System.out.println("REQ STATUS# "+reqStatus);
+//    	  
+//    	  
+//    	  reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//          
+//    	  while ((line = reader.readLine()) != null ){
+//    		  responseContent.append(line);
+//    	  }
+//    	  
+//    	  System.out.println("REQ RESPONSE# "+responseContent);
+//    	   
+//    	  
+//    	   
+//    	   
+//    	   
+//       }catch (Exception e){
+//    	   e.printStackTrace();
+//       }finally {
+//    	   conn.disconnect();
+//	   }
+//	}
+//	
+//	
 	
 	
 	
